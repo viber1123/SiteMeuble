6c65fc6bf59ed46227f1a2fd306bac8f46e8a8c3
Yannick Beyene <yannicklarumeur88@gmail.com>
2023-05-25 05:46:07 +0200

    mise en place du composant Hero

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import components
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="w-full max-w-[1440px] mx-auto bg-white">
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,8 @@
+
+export const hero = {
+  title: " Maison Creative Simplifiez Vos Meubles",
+  subtitle:
+  'Do i have consent to record this meeting gain locaion, root-and-branch, review, nor game plan who’s the goto',
+  buttonText: "Shop Now",
+};
+
